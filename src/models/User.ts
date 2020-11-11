886aa0232aadb19c6fffd8f11c0e869e95847760
EduardoAraD <araujocarlos893@gmail.com>
2020-11-11 12:05:29 -0300

    Update Model User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,22 @@
-export class User {
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export default class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    codeResetPassword: string;
+
+    @Column()
+    codeResetExpires: string;
 }
