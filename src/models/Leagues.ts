96ec153006e41a3aef55ef55e75b04de00ad3f19
EduardoAraD <araujocarlos893@gmail.com>
2020-11-13 19:44:35 -0300

    remove Association League User

--- src/models/Leagues.ts
+++ src/models/Leagues.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany, JoinColumn, OneToOne } from 'typeorm'
 
-import AssociationLeagueUser from './AssociatioLeagueUser'
 import Points from './Points'
 import Users from './Users'
 
@@ -23,10 +22,4 @@ export default class Leagues {
     })
     @JoinColumn({ name: 'leagueId' })
     points: Points[];
-
-    @OneToMany(() => AssociationLeagueUser, association => association.leagueId, {
-      cascade: ['insert', 'update']
-    })
-    @JoinColumn({ name: 'leagueId' })
-    users: AssociationLeagueUser[]
 }
