08c6824e18a4ca3589a6baa9f70c464bc9786a4d
EduardoAraD <araujocarlos893@gmail.com>
2020-11-16 16:08:43 -0300

    Update signIn in ControllerUser

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,9 @@ import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-import Users from '@models/User'
+import Users from '@models/Users'
+import Leagues from '@models/Leagues'
+
 import UsersView from '@views/users_view'
 
 const emailRegex = /\S+@\S+\.\S+/
@@ -49,24 +51,58 @@ export default {
     }
 
     // checando email - DB
-    const usersRepository = getRepository(Users)
+    const UsersRepository = getRepository(Users)
 
-    const userExisting = await usersRepository.findOne({ email: data.email })
+    const userExisting = await UsersRepository.findOne({ email: data.email })
     if (userExisting) {
       return response.status(400).send({ error: 'Email já existente.' })
     }
 
     // criadno o User no DB
+
+    const LeagueRepository = getRepository(Leagues)
+    let PitacoLeague = await LeagueRepository.findOne({ id: 1 })
+    if (!PitacoLeague) {
+      const userPitacoData = {
+        name: 'Pitaco',
+        email: '123456',
+        password: '123456',
+        codeResetExpires: '',
+        codeResetPassword: ''
+      }
+
+      const userPitaco = UsersRepository.create(userPitacoData)
+      await UsersRepository.save(userPitaco)
+      const PitacoLeagueData = {
+        name: 'Pitaco League',
+        dono: userPitaco
+      }
+
+      PitacoLeague = LeagueRepository.create(PitacoLeagueData)
+      await LeagueRepository.save(PitacoLeague)
+    }
+
+    const pointsData = {
+      points: 0,
+      exactScore: 0,
+      leagueId: PitacoLeague
+    }
+
+    const points = []
+    points.push(pointsData)
+
     const userData = {
       name: data.name,
       email: data.email,
       password: passwordHash,
       codeResetPassword: '',
-      codeResetExpires: ''
+      codeResetExpires: '',
+      points: points
     }
 
-    const user = usersRepository.create(userData)
-    await usersRepository.save(user)
+    const user = UsersRepository.create(userData)
+
+    await UsersRepository.save(user)
 
     const token = jwt.sign({ ...user }, process.env.AUTHSECRET as string, {
       expiresIn: '30 day'
