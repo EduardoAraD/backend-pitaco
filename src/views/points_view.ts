313e3521fbac4f16aec843d01b30ed2667a2c449
EduardoAraD <araujocarlos893@gmail.com>
2020-11-17 18:06:21 -0300

    Create PointsView

---
+++ src/views/points_view.ts
@@ -0,0 +1,29 @@
+import Points from '@models/Points'
+
+import usersView from './users_view'
+
+export default {
+  render (point: Points) {
+    return {
+      points: {
+        points: point.points,
+        exactScore: point.exactScore,
+        user: usersView.renderItem(point.userId)
+      }
+    }
+  },
+  renderNotUser (point: Points) {
+    return {
+      points: {
+        points: point.points,
+        exactScore: point.exactScore
+      }
+    }
+  },
+  renderMany (points: Points[]) {
+    return points.map(point => this.render(point))
+  },
+  renderManyNotUser (points: Points[]) {
+    return points.map(point => this.renderNotUser(point))
+  }
+}
