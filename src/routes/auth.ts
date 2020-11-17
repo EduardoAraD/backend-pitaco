201611b712357fbdf77cefb41046234afd7ccf47
EduardoAraD <araujocarlos893@gmail.com>
2020-11-17 18:08:30 -0300

    Update Routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import UsersController from '@controllers/UsersController'
+import LeaguesController from '@controllers/LeaguesController'
 
 const routes = Router()
 
@@ -8,5 +9,11 @@ routes.post('/signup', UsersController.signUp)
 routes.post('/login', UsersController.signIn)
 routes.post('/forgot-password', UsersController.forgotPassword)
 routes.post('/reset-password', UsersController.resetPassword)
+
+routes.get('/leagues', LeaguesController.index)
+routes.get('/leagues/:id', LeaguesController.show)
+routes.post('/league', LeaguesController.create)
+routes.post('/league-dono', LeaguesController.showLeagueDono)
+routes.delete('/leagues/:id', LeaguesController.delete)
 
 export default routes
