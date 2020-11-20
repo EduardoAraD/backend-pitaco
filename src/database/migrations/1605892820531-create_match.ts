564ad1564e3355b3143ebe50d78fe5024f2609b2
EduardoAraD <araujocarlos893@gmail.com>
2020-11-20 15:51:29 -0300

    create DB Match

---
+++ src/database/migrations/1605892820531-create_match.ts
@@ -0,0 +1,85 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createMatch1605892820531 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'match',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
+        {
+          name: 'matchId',
+          type: 'integer'
+        },
+        {
+          name: 'numRodada',
+          type: 'integer'
+        },
+        {
+          name: 'status',
+          type: 'varchar'
+        },
+        {
+          name: 'statium',
+          type: 'varchar'
+        },
+        {
+          name: 'timeHour',
+          type: 'varchar'
+        },
+        {
+          name: 'day',
+          type: 'varchar'
+        },
+        {
+          name: 'clubeHome',
+          type: 'varchar'
+        },
+        {
+          name: 'clubeAway',
+          type: 'varchar'
+        },
+        {
+          name: 'linkClubeHome',
+          type: 'varchar'
+        },
+        {
+          name: 'linkClubeAway',
+          type: 'varchar'
+        },
+        {
+          name: 'golsHome',
+          type: 'integer'
+        },
+        {
+          name: 'golsAway',
+          type: 'integer'
+        },
+        {
+          name: 'rodadaId',
+          type: 'integer'
+        }
+      ],
+      foreignKeys: [
+        {
+          name: 'RodadaMatch',
+          columnNames: ['rodadaId'],
+          referencedTableName: 'rodada',
+          referencedColumnNames: ['id'],
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }
+      ]
+    }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('match')
+  }
+}
