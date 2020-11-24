5d960cd74afb33647b5558f9c04897bc59061be5
EduardoAraD <araujocarlos893@gmail.com>
2020-11-24 11:56:49 -0300

    Updae PitacoView

--- src/views/pitaco_view.ts
+++ src/views/pitaco_view.ts
@@ -1,14 +1,15 @@
 import Pitaco from '@models/Pitaco'
 
 import usersView from './users_view'
+import matchView from './match_view'
 
 export default {
   render (pitaco: Pitaco) {
     return {
-      matchIdApi: pitaco.matchIdApi,
       golsHome: pitaco.golsHome,
       golsAway: pitaco.golsAway,
-      user: usersView.renderItem(pitaco.userId)
+      user: usersView.renderItem(pitaco.userId),
+      match: matchView.renderItem(pitaco.matchId)
     }
   },
 
