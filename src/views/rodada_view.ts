99dd43b3c2b80166b634b70d2f6515f44e7cdc8a
EduardoAraD <araujocarlos893@gmail.com>
2020-11-24 11:57:41 -0300

    Create RodadaView

---
+++ src/views/rodada_view.ts
@@ -0,0 +1,17 @@
+import Rodada from '@models/Rodada'
+
+import matchView from './match_view'
+
+export default {
+  renderItem (rodada: Rodada) {
+    return {
+      name: rodada.name,
+      number: rodada.number,
+      matchs: matchView.renderMany(rodada.matchs)
+    }
+  },
+
+  renderMany (rodadas: Rodada[]) {
+    return rodadas.map(rodada => this.renderItem(rodada))
+  }
+}
