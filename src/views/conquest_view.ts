6fb651c24aaeb830c4d2586ae3b5739db2b5da49
EduardoAraD <araujocarlos893@gmail.com>
2020-12-17 00:12:00 -0300

    Update Conquest e User View

---
+++ src/views/conquest_view.ts
@@ -0,0 +1,13 @@
+import Conquest from '@models/Conquest'
+
+export default {
+  render (conquest: Conquest) {
+    return {
+      position: conquest.position,
+      league: conquest.league
+    }
+  },
+  renderMany (conquests: Conquest[]) {
+    return conquests.map(conquest => this.render(conquest))
+  }
+}
--- src/views/users_view.ts
+++ src/views/users_view.ts
@@ -1,6 +1,8 @@
 import User from '@models/Users'
 
 import pointsView from './points_view'
+import conquestView from './conquest_view'
+import clubeView from './clube_view'
 
 export default {
   render (token: string, user: User, championship: number) {
@@ -11,7 +13,8 @@ export default {
         email: user.email,
         avatar: user.avatar,
         points: pointsView.renderManyNotUser(user.points),
-        heartClub: user.heartClub || {}
+        heartClub: user.heartClub ? clubeView.renderItem(user.heartClub) : {},
+        conquests: conquestView.renderMany(user.conquests)
       },
       championship: championship
     }
@@ -20,7 +23,9 @@ export default {
     return {
       name: user.name,
       email: user.email,
-      avatar: user.avatar
+      avatar: user.avatar,
+      conquests: conquestView.renderMany(user.conquests || []),
+      heartClub: user.heartClub ? clubeView.renderItem(user.heartClub) : {}
     }
   },
   renderMany (users: User[]) {
